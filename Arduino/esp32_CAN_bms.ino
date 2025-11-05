afb8cd7a889a8be21d97c6e588f9ebf803486bd1
Lucas <luke.h.hathaway@gmail.com>
2025-11-05 16:38:55 -0500

    Skeleton

---
+++ Arduino/esp32_CAN_bms.ino
@@ -0,0 +1,34 @@
+// Copyright (c) Sandeep Mistry. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <CAN.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("CAN Sender");
+
+  // start the CAN bus at 500 kbps
+  if (!CAN.begin(500E3)) {
+    Serial.println("Starting CAN failed!");
+    while (1);
+  }
+}
+
+void loop() {
+
+  // send extended packet: id is 29 bits, packet can contain up to 8 bytes of data
+  Serial.print("Sending extended packet ... ");
+
+  CAN.beginExtendedPacket(0x0000008, 8);
+  CAN.write(0xff);
+  CAN.write(0xff);
+  CAN.write(0xff);
+  CAN.endPacket();
+
+  Serial.println("done");
+
+  delay(5000);
+}
+
